0e5cd838b6b9fd700d367eed85ddea21334031aa
Sam Pillsworth <samantha.pillsworth@rubikloud.com>
2018-12-19 10:03:47 -0500

    Flips the symbolic links around

--- .zshenv
+++ .zshenv
@@ -1,1 +1,15 @@
-/Users/sam/.zshenv
+export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:$PATH
+export TERM="screen-256color"
+export ALTERNATE_EDITOR=""
+export EDITOR="emacsclient -t"
+
+export GOPATH="/usr/local/Cellar/go/1.8/"
+export GOBIN=$GOPATH/bin
+#export PATH=$PATH:$GOPATH/bin
+
+export TEXBIN="/Library/TeX/texbin"
+export PATH=/usr/local/opt/sqlite/bin:$PATH
+export PATH=/usr/local/src/anaconda3/bin:$PATH:$GOBIN:$TEXBIN:~/.local/bin
+export PATH=/usr/local/share/dotnet:$PATH
+
+export PYTHONPATH=/usr/local/Cellar/apache-spark/2.1.1/libexec/python:/usr/local/Cellar/apache-spark/2.1.1/libexec/python/build:$PYTHONPATH
--- dot-emacs/custom.el
+++ dot-emacs/custom.el
@@ -1,1 +1,17 @@
-/Users/sam/.emacs.d/custom.el
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-enforce-todo-checkbox-dependencies nil nil nil "Customized with use-package org-agenda")
+ '(org-enforce-todo-dependencies t nil nil "Customized with use-package org-agenda")
+ '(org-todo-keywords
+   (quote
+    ((sequence "TODO(t)" "NEXT(n)" "STARTED(s)" "DEADLINE(D)" "|" "DONE(d!)")
+     (sequence "HOLD(h!)" "WAITING(w@/!)" "|" "CANCELLED(c@/!)" "MEETING(m)")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
