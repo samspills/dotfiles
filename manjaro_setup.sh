9122d3427d64455d1198dcfd7a5a60a841b97475
Sam Pillsworth <srpillsworth@gmail.com>
2019-03-16 16:46:38 -0400

    [i3] [manjaro] add package list + install scripts to repo, refactors

---
+++ manjaro_setup.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+sudo pacman -Syyu
+sudo pacman -Syu --noconfirm $(cat packages.txt | awk '{print $1}')
+sudo pacman -Rs $(pacman -Qdtq)
+
+if [ ! -d ~/.password-store ]; then
+    echo "setting up password-store"
+    git clone git@gitlab.com:samspills/passwords ~/.password-store
+    pass init --path=~/.password-store Sam Pillsworth
+    pass git init
+fi
+
+if [ ! -f ~/.antigen.zsh ]; then
+    echo "fetching antigen"
+    curl -L git.io/antigen > ~/.antigen.zsh
+fi
+
+if [ ! -d ~/dotfiles ]; then
+    echo "fetching dotfiles repo"
+    git clone git@github.com:samspills/dotfiles /home/sam/dotfiles
+    ln -s /home/sam/dotfiles/dot-emacs/.spacemacs /home/sam/.spacemacs
+fi
+
+if [ ! -d ~/.emacs.d ]; then
+    echo "fetching spacemacs fork"
+    git clone git@github.com:samspills/spacemacs ~/.emacs.d
+    ln -s /home/sam/dotfiles/dot-emacs/custom.el /home/sam/.emacs.d/custom.el
+fi
+
+if [ ! "$SHELL" = "$(which zsh)" ]; then
+    chsh -s $(which zsh)
+fi
+
+if [ ! -d ~/.etc/rofi-pass ]; then
+    echo "fetching rofi-pass"
+    git clone git@github.com:carnager/rofi-pass.git ~/.etc/rofi-pass
+fi
