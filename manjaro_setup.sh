e022d33bdc3eb106eb93cb85cedf7a3627a664bf
Sam Pillsworth <srpillsworth@gmail.com>
2020-06-19 07:54:35 -0400

    manjaro

--- manjaro_setup.sh
+++ manjaro_setup.sh
@@ -3,6 +3,13 @@ sudo pacman -Syyu
 sudo pacman -Syu --noconfirm $(cat packages.txt | awk '{print $1}')
 sudo pacman -Rs $(pacman -Qdtq)
 
+if [ ! -d ~/.password-store ]; then
+    echo "setting up password-store"
+    git clone git@gitlab.com:samspills/passwords ~/.password-store
+    pass init --path=~/.password-store Sam Pillsworth
+    pass git init
+fi
+
 if [ ! -f ~/.antigen.zsh ]; then
     echo "fetching antigen"
     curl -L git.io/antigen > ~/.antigen.zsh
@@ -10,13 +17,13 @@ fi
 
 if [ ! -d ~/dotfiles ]; then
     echo "fetching dotfiles repo"
-    git clone https://github.com/samspills/dotfiles /home/sam/dotfiles
+    git clone git@github.com:samspills/dotfiles /home/sam/dotfiles
     ln -s /home/sam/dotfiles/dot-emacs/.spacemacs /home/sam/.spacemacs
 fi
 
 if [ ! -d ~/.emacs.d ]; then
     echo "fetching spacemacs fork"
-    git clone https://github.com/samspills/spacemacs ~/.emacs.d
+    git clone git@github.com:samspills/spacemacs ~/.emacs.d
     ln -s /home/sam/dotfiles/dot-emacs/custom.el /home/sam/.emacs.d/custom.el
 fi
 
@@ -26,7 +33,7 @@ fi
 
 if [ ! -d ~/.etc/rofi-pass ]; then
     echo "fetching rofi-pass"
-    git clone https://github.com/carnager/rofi-pass.git ~/.etc/rofi-pass
+    git clone git@github.com:carnager/rofi-pass.git ~/.etc/rofi-pass
     ln -s ~/.etc/rofi-pass/rofi-pass ~/bin/rofi-pass
     ln -s ~/.etc/rofi-pass/addpass ~/bin/addpass
 fi
